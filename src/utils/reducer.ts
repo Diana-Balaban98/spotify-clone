e58af8ece8fbc0671d1d3ccb81f3dc85ea474da7
Diana-Balaban98 <diana.rud.2012@mail.ru>
2023-07-23 23:31:25 +0300

    first commit

---
+++ src/utils/reducer.ts
@@ -0,0 +1,11 @@
+import internal from "stream";
+
+export const initialState = {
+    token: null,
+}
+
+export const reducer = (state = initialState, action: any) => {
+    switch (action.type) {
+        default: return state
+    }
+}
